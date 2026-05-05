7c3a5e7b41daf825a1e9bf0067ecad53775b988a
hxf <hxf@project.dev>
2026-05-05 20:57:51 +0800

    feat: add UnifiedPagination shared component

---
+++ src/components/common/UnifiedPagination.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="pagination-wrapper">
+    <span>共{{ total }}条</span>
+    <el-pagination
+      background
+      layout="prev, pager, next, jumper, sizes"
+      :total="total"
+    />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  total: { type: Number, default: 360 }
+})
+</script>
+
+<style lang="scss" scoped>
+.pagination-wrapper {
+  margin-top: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+  color: #606266;
+  font-size: 13px;
+}
+</style>
